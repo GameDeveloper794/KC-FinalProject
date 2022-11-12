e36d0c8b032e67e7dce2df3b3ae77d36197fd44a
محمود عماد غزلان <114683395+GameDeveloper794@users.noreply.github.com>
2022-11-12 20:43:04 +0300

    update

---
+++ Escape World Extinction/Assets/AI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public Transform Player;
+    UnityEngine.AI.NavMeshAgent agent;
+    public float attackRadius = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.position);
+        if (distance < attackRadius)
+        agent.SetDestination(Player.position);
+    }
+}
